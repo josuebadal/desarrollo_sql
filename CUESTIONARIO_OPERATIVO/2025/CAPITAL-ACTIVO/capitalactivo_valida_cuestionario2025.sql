fe94226e40d600a15d5f5de6ca6eec0e3a3bf61d
JOSUE BADAL <josuebadal.jjbc@gmail.com>
2026-07-08 18:26:30 -0600

    querys para validacion de los cuestionarios

--- CUESTIONARIO_OPERATIVO/2025/CAPITAL-ACTIVO/capitalactivo_valida_cuestionario2025.sql
+++ CUESTIONARIO_OPERATIVO/2025/CAPITAL-ACTIVO/capitalactivo_valida_cuestionario2025.sql
@@ -1,19 +1,3 @@
-/*Se deberan generar los cuestinarios 1 x 1
-mediante las siguientes consultas
-
-select * from cuestionario_a1(29037,2025);
-
-select * from cuestionario_a2(12345,2025);
-
-select * from cuestionario_b(12345,2025);
-
-select * from cuestionario_c_2026(12345,2025);
-
-select * from cuestionario_d1(12345,2025);
-
-select * from cuestionario_d2(12345,2025);
-*/
-
 -----------------------------------------------------------
 --                    VALIDACIONES PARA CUESTIONARIO A1
 -- Tabla temporal: a1_cuestionario_operatividad
@@ -24,6 +8,7 @@ tmp_act
 temp_peps
 tmp_act_peps
 
+-----VALIDA TRABAJOS SIN ACTIVIDAD ECONOMICAS-----
 SELECT tr.idorigen,tr.idgrupo,tr.idsocio, tr.actividad_economica_pld 
 FROM tmp_act as t
 INNER JOIN trabajo as tr ON t.idorigen = tr.idorigen
@@ -42,25 +27,10 @@ INNER JOIN trabajo as tr ON t.idorigen = tr.idorigen
 AND t.idgrupo = tr.idgrupo AND t.idsocio = tr.idsocio AND tr.consecutivo= 1
 WHERE tr.actividad_economica_pld IS NULL;
 
------VALIDAR QUE NINGUN DATO VENGA VACIO O QUE SE REPORTE LA CLAVE DE EJEMPLO 12345
-SELECT * FROM a1_cuestionario_operatividad
-where anio IS NULL OR  anio != '2025' OR clave_formulario IS NULL 
-OR  clave_de_entidad = '12345'
-OR tipo_cliente_o_usuario IS NULL OR clasificacion_grado_riesgo IS NULL
-OR pais_nacionalidad IS NULL OR pais_residencia IS NULL 
-OR entidad_federativa_residencia IS NULL 
-OR actividad_economica is null OR numero_total_clientes IS NULL 
-;
-
---OBSERVACIONES CONUMEX ----------------
 ----CUESTIONARIO A1----------------
 ------VALIDA NUMERO TOTAL DE SOCIOS QUE APLICAN AL FORMATO
---------LOS SOCIOS QUE SE INCLUYEN
 SELECT * from tmp_act;
 select COUNT(*) from tmp_act;
-
---------TOTAL DE  SOCIOS QUE SE INCLUYEN
-select SUM(numero_total_clientes::integer) from a1_cuestionario_operatividad;
 
 --------VALIDA TOTAL DE SOCIOS SIN TRABAJO
 select count(*)
@@ -71,10 +41,6 @@ and ax.idgrupo  = tr.idgrupo
 and ax.idsocio  = tr.idsocio
 and tr.consecutivo = 1
 where tr.idsocio is null;
- count 
--------
-    58
-(1 fila)
 
 select ax.*
 from tmp_act ax
@@ -84,18 +50,6 @@ and ax.idgrupo = tr.idgrupo
 and ax.idsocio = tr.idsocio
 and tr.consecutivo = 1
 where tr.idorigen is null;
-
-
-------VALIDA CLASIFICACION POR GRADO DE RIESGO
-El grado de Riesgo también es incorrecto ya que no manejamos grado de riesgo 3, solo manejamos 1 y 5.
---NO DEBE SALIR clasificacion_grado_riesgo    | 3
-SELECT * from a1_cuestionario_operatividad where clasificacion_grado_riesgo::numeric != 1;
-
-------VALIDA CLASIFICACION POR GRADO DE RIESGO
-PEP: Reportan 112 PEP lo que es un dato erróneo
-
-Se estan reportando 272 haciendo un filtro en la columna D y contabilizando la columna J 
-
 
 ------VALIDA ENTIDAD FEDERATIVA RESIDENCIA H
 SELECT tact.idorigen, tact.idgrupo, tact.idsocio, est.idestado, est.nombre,tr.consecutivo, tr.actividad_economica_pld
@@ -109,24 +63,73 @@ inner join estados as est ON mun.idestado = est.idestado
 WHERE tr.actividad_economica_pld is null or tr.actividad_economica_pld = ''
 ;
 
-
-
-
-nuevomexico31dic25_cuestionario=# 
-nuevomexico31dic25_cuestionario=# select sum(monto + montoio + montoim + montoiva + montoivaim) from auxiliares_d_h ad 
+ select sum(monto + montoio + montoim + montoiva + montoivaim) from auxiliares_d_h ad 
          inner join auxiliares_h a using (idorigenp,idproducto,idauxiliar)
          inner join temp_productos te using(idproducto)
          inner join tmp_clientes ta using (idorigen, idgrupo, idsocio)
          inner join polizas        po using (idorigenc,periodo,idtipo,idpoliza)
          where ad.tipomov = 0 and ad.idtipo in (1,2) 
          and ad.periodo:: integer between 202501 and 202512;
-     sum      
---------------
  130258464.91
 (1 fila)
 
 nuevomexico31dic25_cuestionario=# select 554247034.28+130258464.91;
-   ?column?   
---------------
  684505499.19
 (1 fila)
+
+---------- VALIDACION  JULIO 2026 ----------
+----- ERROR AL REPORTAR A LA SIGUIENTE PERSONA EXTRANJERA COMO PF-NACIONAL TIPO 3
+tipo_cliente_o_usuario	                5
+clasificacion_grado_riesgo	            1
+pais_nacionalidad	                    47
+pais_residencia	                        47
+entidad_federativa_residencia	        2
+act_eco_pld	numero_total_clientes       43322
+
+SELECT tact.idorigen, tact.idgrupo, tact.idsocio, 
+est.idestado, est.nombre,
+p.lugarnacimiento,p.nacionalidad,p.razon_social,
+tr.consecutivo, tr.actividad_economica_pld
+FROM tmp_act as tact
+inner join personas as p on tact.idorigen = p.idorigen AND tact.idgrupo = p.idgrupo AND tact.idsocio = p.idsocio
+inner join trabajo as tr on tact.idorigen = tr.idorigen AND tact.idgrupo = tr.idgrupo AND tact.idsocio = tr.idsocio AND consecutivo = 1
+inner join colonias as col ON p.idcolonia = col.idcolonia
+inner join municipios as mun ON col.idmunicipio = mun.idmunicipio
+inner join estados as est ON mun.idestado = est.idestado
+where p.pais_nacimiento = 43  and p.nacionalidad  = 3;
+idorigen                | 20110
+idgrupo                 | 10
+idsocio                 | 396
+idestado                | 2
+nombre                  | Baja California Norte
+lugarnacimiento         | colombia
+nacionalidad            | 3
+razon_social            | 
+consecutivo             | 1
+actividad_economica_pld | 0043322
+
+-----SEGUNDO ERROR 
+
+
+SELECT tact.idorigen, tact.idgrupo, tact.idsocio,
+col.idcolonia, col.nombre, 
+mun.idmunicipio, mun.nombre,
+est.idestado, est.nombre,
+pa.idpais, pa.nombre,
+p.lugarnacimiento,p.nacionalidad,p.razon_social,
+tr.consecutivo, tr.actividad_economica_pld
+FROM tmp_act as tact
+inner join personas as p on tact.idorigen = p.idorigen AND tact.idgrupo = p.idgrupo AND tact.idsocio = p.idsocio
+inner join trabajo as tr on tact.idorigen = tr.idorigen AND tact.idgrupo = tr.idgrupo AND tact.idsocio = tr.idsocio AND consecutivo = 1
+inner join colonias as col ON p.idcolonia = col.idcolonia
+inner join municipios as mun ON col.idmunicipio = mun.idmunicipio
+inner join estados as est ON mun.idestado = est.idestado
+inner join paises  as pa on est.idpais = pa.idpais
+where p.nacionalidad  = 3;
+
+
+-----CAMBIO DE PASWWORD -----
+update usuario set passwd = md5('247cambio')
+where idusuario = 247;
+
+DELETE intentos_fallidos where idusuario = 247;
