a254b7ca96df128e62cf88dedec854f00e636c7f
JOSUE BADAL <josuebadal.jjbc@gmail.com>
2026-07-10 18:52:23 -0600

    avances en la matriz ebr de buenosaires

--- Querys_Cajas/PDNTS/buenos_aires/cheques/matriz_ebr_buenosaires.sql
+++ Querys_Cajas/PDNTS/buenos_aires/cheques/matriz_ebr_buenosaires.sql
@@ -1,7 +1,25 @@
-SELECT p.appaterno||'-'||p.apmaterno||'-'||p.nombre as socio,
+SELECT p.appaterno||'-'||p.apmaterno||'-'||p.nombre as socio, 
+(case when p.estatus = true then 'activo' else 'inactivo' end) as estatus,
+(case when p.nivel_riesgo = 1 then 'bajo' else 'alto' end)     as nries,
 a.idorigen||'-'||a.idgrupo||'-'||a.idsocio AS ogs,
 a.idorigenp||'-'||a.idproducto||'-'||a.idauxiliar AS opa,
-pr.nombre as producto,ad.fecha, --ad.cargoabono,
+(case   when ad.idtipo = 1 then 'ventanilla'
+        when ad.idtipo = 2 then 'cheque'
+        when ad.idtipo = 3 then 'traspaso'
+        else 'ND' end) as tipo,
+(case   when ad.tipomov = 0 then 'cargos-abonos'
+        when ad.tipomov = 1 then 'castigos'
+        when ad.tipomov = 2 then 'quita'
+        when ad.tipomov = 3 then 'condonacion'
+        when ad.tipomov = 4 then 'bonificacion'
+        when ad.tipomov = 5 then 'descuento'
+        when ad.tipomov = 6 then 'ajuste'
+        else 'ND' end ) as tipomov,
+pr.nombre as producto,ad.fecha, ad.monto, ad.montoio, ad.montoim, ad.montoiva,
+(case 
+        when ad.cargoabono = 0 then 'cargo'
+        else 'abono' end ) as cargoabono,
+'creditos' as tipoprod,
 ad.idorigenc||'-'||ad.periodo||'-'||ad.idtipo||'-'||ad.idpoliza as poliza
 FROM v_auxiliares AS a
 INNER JOIN personas AS p
@@ -10,16 +28,33 @@ INNER JOIN productos as pr
 ON a.idproducto = pr.idproducto
 INNER JOIN v_auxiliares_d AS ad
 ON  a.idorigenp = ad.idorigenp AND a.idproducto = ad.idproducto AND a.idauxiliar = ad.idauxiliar
-WHERE a.estatus = 2
-AND a.idproducto BETWEEN 30000 AND 39999 AND ad.tipomov = 0 AND ad.cargoabono = 1 AND ad.idtipo = 2
-ORDER BY ad.fecha DESC;
+WHERE a.idproducto BETWEEN 30000 AND 39999
+AND ad.fecha BETWEEN '2025-07-01 00:00:00' AND '2026-06-30 23:59:59'
 
-UNION 
+UNION ALL
 
-SELECT p.appaterno||'-'||p.apmaterno||'-'||p.nombre as socio,
+SELECT p.appaterno||'-'||p.apmaterno||'-'||p.nombre as socio, 
+(case when p.estatus = true then 'activo' else 'inactivo' end) as estatus,
+(case when p.nivel_riesgo = 1 then 'bajo' else 'alto' end)     as nries,
 a.idorigen||'-'||a.idgrupo||'-'||a.idsocio AS ogs,
 a.idorigenp||'-'||a.idproducto||'-'||a.idauxiliar AS opa,
-pr.nombre as producto,ad.fecha, --ad.cargoabono,
+(case   when ad.idtipo = 1 then 'ventanilla'
+        when ad.idtipo = 2 then 'cheque'
+        when ad.idtipo = 3 then 'traspaso'
+        else 'ND' end) as tipo,
+(case   when ad.tipomov = 0 then 'cargos-abonos'
+        when ad.tipomov = 1 then 'castigos'
+        when ad.tipomov = 2 then 'quita'
+        when ad.tipomov = 3 then 'condonacion'
+        when ad.tipomov = 4 then 'bonificacion'
+        when ad.tipomov = 5 then 'descuento'
+        when ad.tipomov = 6 then 'ajuste'
+        else 'ND' end ) as tipomov,
+pr.nombre as producto,ad.fecha, ad.monto, ad.montoio, ad.montoim, ad.montoiva,
+(case 
+        when ad.cargoabono = 1 then 'cargo'
+        else 'abono' end ) as cargoabono,
+'creditos' as tipoprod,
 ad.idorigenc||'-'||ad.periodo||'-'||ad.idtipo||'-'||ad.idpoliza as poliza
 FROM v_auxiliares AS a
 INNER JOIN personas AS p
@@ -28,9 +63,6 @@ INNER JOIN productos as pr
 ON a.idproducto = pr.idproducto
 INNER JOIN v_auxiliares_d AS ad
 ON  a.idorigenp = ad.idorigenp AND a.idproducto = ad.idproducto AND a.idauxiliar = ad.idauxiliar
-WHERE a.estatus = 2
-AND pr.cuentaaplica like '201%' 
-AND ad.tipomov = 0 
-AND ad.cargoabono = 0 
-AND ad.idtipo = 2
-ORDER BY ad.fecha DESC;
+WHERE pr.cuentaaplica like '201%' 
+AND ad.fecha BETWEEN '2025-07-01 00:00:00' AND '2026-06-30 23:59:59'
+;
--- Querys_Cajas/PDNTS/leonfranco/bajas_grupo_88/notas_q128.sql
+++ Querys_Cajas/PDNTS/leonfranco/bajas_grupo_88/notas_q128.sql
@@ -1,3 +1,18 @@
+Buenas tardes.
+Podrian apoyarnos para agregar al query 123 
+el ahorro adulto (110), 
+el ahorro adicional (111) , 
+ahorro cuenta corriente (130), 
+ahorro cuenta confianza (131) 
+además de si tiene inversiones (depósitos a plazo fijo) (200,201,202,203) 
+con los que cuentan los socios que salen en este reporte.
+Gracias de antemano por el apoyo brindado saludos cordiales.
+
+
+
+
+
+
 --SE TRABAJA LA BASE DE DATOS 31 DE MAYO 2026
 --PARA CARGAR INFO ES EL QUERY  121
 
